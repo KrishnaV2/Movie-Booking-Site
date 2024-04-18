20807d380828b99b86a51a00dd179ff7232ad05b
Krishna Vijaywargia <95163831+KrishnaV2@users.noreply.github.com>
2024-04-18 11:18:10 +0530

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -6,7 +6,7 @@ function About() {
         <div>
             <Header />
             <div className="bg-slate-300 md:min-h-[92.7vh] min-h-screen">
-                <div className='border-2 border-red-500 p-4 md:w-2/3 text-center w-full m-auto font-medium text-2xl'>
+                <div className='p-4 md:w-2/3 text-center w-full m-auto font-medium text-2xl'>
                     <div className='py-5'>
                         A fully responsive movie ticket booking website made using ReactJS and TailwindCSS. The route "/booking" is the same for all movies. It displays data dynamically based on the movie chosen by the user.
                     </div>
